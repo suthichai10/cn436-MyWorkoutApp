eec2f35445515db8ae24a6bc2e0a2e6470ec5c26
Suthichai Chukaew <suthichai.chu@dome.tu.ac.th>
2021-09-13 05:43:41 -0700

    add Exercise Model

---
+++ MyWorkOut/Model/Exercise.swift
@@ -0,0 +1,51 @@
+//
+//  Exercise.swift
+//  MyWorkOut
+//
+//  Created by suthichai on 13/9/2564 BE.
+//
+
+import SwiftUI
+
+struct Exercise {
+    let exerciseName : String
+    let videoName : String
+    
+    enum ExerciseEnum {
+        case Squat
+        case Burpee
+        case Lunges
+        case ObiqueAndCore
+        case Planks
+        case Pushups
+        
+        var description : String {
+            switch self {
+            case .Squat:
+                return NSLocalizedString("Squart", comment: "exercise")
+            case .Burpee:
+                return NSLocalizedString("Burpee", comment: "exercise")
+            case .Lunges:
+                return NSLocalizedString("Lunges", comment: "exercise")
+            case .ObiqueAndCore:
+                return NSLocalizedString("ObiqueAndCore", comment: "exercise")
+            case .Planks:
+                return NSLocalizedString("Planks", comment: "exercise")
+            case .Pushups:
+                return NSLocalizedString("Pushups", comment: "exercise")
+            }
+        }
+    }
+}
+
+
+extension Exercise {
+    static let exercise = [
+        Exercise(exerciseName: String(describing: ExerciseEnum.Squat), videoName: "Squat"),
+        Exercise(exerciseName: String(describing: ExerciseEnum.Burpee), videoName: "Burpee"),
+        Exercise(exerciseName: String(describing: ExerciseEnum.Lunges), videoName: "Lunges"),
+        Exercise(exerciseName: String(describing: ExerciseEnum.ObiqueAndCore), videoName: "ObiqueAndCore"),
+        Exercise(exerciseName: String(describing: ExerciseEnum.Planks), videoName: "Planks"),
+        Exercise(exerciseName: String(describing: ExerciseEnum.Pushups), videoName: "Pushups")
+    ]
+}
