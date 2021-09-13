1b90e5f0aea0754764b7e65faa9f0d42cca83604
Suthichai Chukaew <suthichai.chu@dome.tu.ac.th>
2021-09-13 05:19:08 -0700

    initial commit

---
+++ MyWorkOut/View/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  MyWorkOut
+//
+//  Created by suthichai on 11/9/2564 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
