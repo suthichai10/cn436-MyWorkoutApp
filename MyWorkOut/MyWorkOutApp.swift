17b1e3f7928f65ed9b31f6e6e8a1007b7dacb0f5
Suthichai Chukaew <suthichai.chu@dome.tu.ac.th>
2021-09-13 05:44:55 -0700

    add MyWorkoutApp

---
+++ MyWorkOut/MyWorkOutApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWorkOutApp.swift
+//  MyWorkOut
+//
+//  Created by suthichai on 11/9/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct MyWorkOutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
