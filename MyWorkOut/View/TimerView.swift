708e7f14961365c65fa523e6eb0ee09bdabe1691
Suthichai Chukaew <suthichai.chu@dome.tu.ac.th>
2021-09-14 06:38:11 -0700

    add TimerView

--- MyWorkOut/View/TimerView.swift
+++ MyWorkOut/View/TimerView.swift
@@ -5,4 +5,31 @@
 //  Created by suthichai on 14/9/2564 BE.
 //
 
-import Foundation
+import SwiftUI
+
+struct TimerView : View {
+    @State private var timeRemaining = 3
+    @Binding var timerDone : Bool
+    var timerFontSize : CGFloat
+    let timeColor = [Color.red , Color.green , Color.blue]
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    var body : some View {
+        Text("\(timeRemaining)")
+            .font(.system(size: timerFontSize,design: .rounded))
+            .onReceive(timer, perform: { _ in
+                if timeRemaining > 1 {
+                    timeRemaining -= 1
+                } else {
+                    timerDone = true
+                }
+            })
+            .foregroundColor(timeColor[timeRemaining - 1])
+    }
+}
+
+struct TimerView_Preview : PreviewProvider {
+    static var previews: some View {
+        TimerView(timerDone: .constant(true), timerFontSize: 90)
+    }
+}
+
