f4104890f5cfd2f2f4422839238f0f4eac56de5d
Suthichai Chukaew <suthichai.chu@dome.tu.ac.th>
2021-09-14 06:39:15 -0700

    update ExerciseView

--- MyWorkOut/View/ExerciseView.swift
+++ MyWorkOut/View/ExerciseView.swift
@@ -9,22 +9,46 @@ import SwiftUI
 import AVKit
 
 struct ExerciseView : View {
+    @State private var timerDone = false
+    @State private var showTimer = false
     @Binding var selectedTab : Int
     let index : Int
     var body : some View {
         GeometryReader { geometry in
-            VStack {
-                Text(Exercise.exercise[index].exerciseName)
-                if let url = Bundle.main.url(forResource: Exercise.exercise[index].videoName, withExtension: "mp4") {
-                    VideoPlayer(player: AVPlayer(url: url))
-                        .frame(width: geometry.size.width , height: geometry.size.height * 0.45)
+            VStack(alignment: .center) {
+                Text(Exercise.exercise[index].exerciseName).font(.largeTitle)
+                if let url = Bundle.main.url(forResource: Exercise.exercise[index].videoName, withExtension: "mp4") , let player = AVPlayer(url: url) {
+                    VideoPlayer(player: player)
+                        .frame(width: geometry.size.width, height: geometry.size.height * 0.45)
+                    HStack {
+                        Spacer()
+                        Button(NSLocalizedString("Start", comment: "Start")) {
+                            //player.play()
+                            showTimer = true
+                        }.border(Color.black)
+                        Spacer()
+                        Button(NSLocalizedString("Done", comment: "Done")) {
+                            selectedTab += 1
+                        }.border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
+                        .disabled(!timerDone)
+                        Spacer()
+                    }
                 } else {
-                    Text("Couldn't find \(Exercise.exercise[index].videoName).mp4")
+                    Text(NSLocalizedString("Couldn't find \(Exercise.exercise[index].videoName).mp4", comment: "Couldn't find video"))
                         .foregroundColor(.red)
                 }
-                Button("Next") {
-                    selectedTab += 1
+                Spacer()
+                if showTimer {
+                    TimerView(timerDone: $timerDone, timerFontSize: geometry.size.height * 0.15)
+                } else {
+                    Text("0")
+                        .foregroundColor(.white)
+                        .font(.system(size: geometry.size.height * 0.15,design: .rounded))
                 }
+                Spacer()
+                RatingView()
+                Spacer()
+                FooterView(selectedTab:$selectedTab)
             }
         }
     }
