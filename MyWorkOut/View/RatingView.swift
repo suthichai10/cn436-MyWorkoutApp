3ba0c21b64056f9cd58dacda30ab2ae5098c08a2
Suthichai Chukaew <suthichai.chu@dome.tu.ac.th>
2021-09-14 06:37:33 -0700

    add RatingView

--- MyWorkOut/View/RatingView.swift
+++ MyWorkOut/View/RatingView.swift
@@ -9,12 +9,17 @@ import SwiftUI
 
 struct RatingView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            ForEach(0..<5) { _ in
+                Image(systemName: "star")
+            }
+        }
     }
 }
 
 struct RatingView_Previews: PreviewProvider {
     static var previews: some View {
         RatingView()
+            .previewLayout(.sizeThatFits)
     }
 }
