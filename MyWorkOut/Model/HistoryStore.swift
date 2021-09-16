6e5f257e08e1d8d521f16326489549d402494736
Suthichai Chukaew <suthichai.chu@dome.tu.ac.th>
2021-09-16 10:38:22 -0700

    add HistoryStore

--- MyWorkOut/Model/HistoryStore.swift
+++ MyWorkOut/Model/HistoryStore.swift
@@ -5,16 +5,21 @@
 //  Created by suthichai on 15/9/2564 BE.
 //
 
-import SwiftUI
+import Foundation
 
-struct HistoryStore: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct HistoryStore {
+    var exerciseDays : [ExerciseDay] = []
+    
+    init() {
+        #if DEBUG
+        createDevData()
+        #endif
     }
+
 }
 
-struct HistoryStore_Previews: PreviewProvider {
-    static var previews: some View {
-        HistoryStore()
-    }
+struct ExerciseDay : Identifiable {
+    let id = UUID()
+    let date : Date
+    var exercises  : [String]
 }
