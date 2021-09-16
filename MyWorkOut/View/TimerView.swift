6146ecaad35287c51475f1b1b5bc0b92ed86a3ef
Suthichai Chukaew <suthichai.chu@dome.tu.ac.th>
2021-09-16 10:40:54 -0700

    update Time Color

--- MyWorkOut/View/TimerView.swift
+++ MyWorkOut/View/TimerView.swift
@@ -11,19 +11,19 @@ struct TimerView : View {
     @State private var timeRemaining = 3
     @Binding var timerDone : Bool
     var timerFontSize : CGFloat
-    let timeColor = [Color.red , Color.green , Color.blue]
+    let timeColor = [Color.white , Color.red , Color.green , Color.blue]
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     var body : some View {
         Text("\(timeRemaining)")
             .font(.system(size: timerFontSize,design: .rounded))
             .onReceive(timer, perform: { _ in
-                if timeRemaining > 1 {
+                if timeRemaining > 0 {
                     timeRemaining -= 1
                 } else {
                     timerDone = true
                 }
             })
-            .foregroundColor(timeColor[timeRemaining - 1])
+            .foregroundColor(timeColor[timeRemaining])
     }
 }
 
