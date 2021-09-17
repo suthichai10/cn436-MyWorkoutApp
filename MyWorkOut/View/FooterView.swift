7a3e3bd20d96aef175d45cf3899f9fde0bf9f796
Suthichai Chukaew <suthichai.chu@dome.tu.ac.th>
2021-09-17 05:38:41 -0700

    update FooterView

--- MyWorkOut/View/FooterView.swift
+++ MyWorkOut/View/FooterView.swift
@@ -8,18 +8,30 @@
 import SwiftUI
 
 struct FooterView: View {
+    @EnvironmentObject private var history : HistoryStore
     @Binding var selectedTab : Int
     @Binding var showHistory : Bool
     var body: some View {
         VStack {
+            let today = history.getTodayExerciseData()
             HStack {
                 ForEach(0..<Exercise.exercise.count) { index in
                     let fill = (index == selectedTab) ? ".fill" : ""
-                    Image(systemName: "\(index + 1).circle\(fill)")
-                        .onTapGesture {
-                            selectedTab = index
-                        }
-                        .font(.largeTitle)
+                    if today?.exercises != nil , today!.exercises.contains(Exercise.exercise[index].exerciseName) {
+                        Image(systemName: "\(index + 1).circle\(fill)")
+                            .onTapGesture {
+                                selectedTab = index
+                            }
+                            .font(.largeTitle)
+                            .foregroundColor(.green)
+                    } else {
+                        Image(systemName: "\(index + 1).circle\(fill)")
+                            .onTapGesture {
+                                selectedTab = index
+                            }
+                            .font(.largeTitle)
+                            .foregroundColor(.red)
+                    }
                 }
             }
             Button(NSLocalizedString("History", comment: "History")) {
@@ -34,5 +46,6 @@ struct FooterVIew_Previews: PreviewProvider {
     static var previews: some View {
         FooterView(selectedTab: .constant(0), showHistory: .constant(false))
             .previewLayout(.sizeThatFits)
+            .environmentObject(HistoryStore())
     }
 }
