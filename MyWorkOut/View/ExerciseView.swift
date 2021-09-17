8e1545b96ab0916f9d6f681a805417ef017c05ec
Suthichai Chukaew <suthichai.chu@dome.tu.ac.th>
2021-09-17 05:41:39 -0700

    update ExerciseView

--- MyWorkOut/View/ExerciseView.swift
+++ MyWorkOut/View/ExerciseView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 import AVKit
 
 struct ExerciseView : View {
+    @EnvironmentObject private var history  : HistoryStore
     @State private var timerDone = false
     @State private var showTimer = false
     @State private var showHistory = false
@@ -18,7 +19,7 @@ struct ExerciseView : View {
     var body : some View {
         GeometryReader { geometry in
             VStack(alignment: .center) {
-                Text(Exercise.exercise[index].exerciseName).font(.largeTitle)
+                Text(Exercise.exercise[index].exerciseName).font(.title)
                 if let url = Bundle.main.url(forResource: Exercise.exercise[index].videoName, withExtension: "mp4") , let player = AVPlayer(url: url) {
                     VideoPlayer(player: player)
                         .frame(width: geometry.size.width, height: geometry.size.height * 0.45)
@@ -30,12 +31,18 @@ struct ExerciseView : View {
                         }.border(Color.black)
                         Spacer()
                         Button(NSLocalizedString("Done", comment: "Done Button")) {
+                            history.addExercise(exercise: Exercise.exercise[index].exerciseName)
                             if selectedTab + 1 == Exercise.exercise.count {
+                                let today = history.getTodayExerciseData()
+                                if today!.exercises.count != Exercise.exercise.count {
+                                    let index = history.getFirstIncompleteExerciseIndex()!
+                                    selectedTab = index
+                                    return
+                                }
                                 showSuccess = true
-                            } else {
-                                selectedTab += 1
+                                return
                             }
-
+                            selectedTab += 1
                         }
                         .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
                         .disabled(!timerDone)
@@ -54,12 +61,14 @@ struct ExerciseView : View {
                         .font(.system(size: geometry.size.height * 0.15,design: .rounded))
                 }
                 Spacer()
-                RatingView(rating: AppStorage(wrappedValue:  1, "ratingForExercise\(index + 1)"))
+                RatingView(rating: AppStorage(wrappedValue:  0, "ratingForExercise\(index + 1)"))
                 Spacer()
                 FooterView(selectedTab:$selectedTab , showHistory: $showHistory)
+                    .environmentObject(history)
             }
             .fullScreenCover(isPresented: $showHistory) {
                 HistoryView()
+                    .environmentObject(history)
             }
             .fullScreenCover(isPresented: $showSuccess) {
                 SuccessView(selectedTab: $selectedTab)
@@ -71,5 +80,6 @@ struct ExerciseView : View {
 struct ExerciseView_Preview : PreviewProvider {
     static var previews: some View {
         ExerciseView(selectedTab: .constant(1) , index: 0)
+            .environmentObject(HistoryStore())
     }
 }
